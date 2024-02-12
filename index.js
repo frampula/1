7cfe0d944aa979ed831b9c0b0c220806bee16d1f
framupla <kidalo1337@gmail.com>
2024-02-12 18:22:00 +0200

    Farenheit in Celvin

--- index.js
+++ index.js
@@ -1,45 +1,16 @@
-const CURRENCY = {
-  USD_TO_UAH: 37.65,
-  EUR_TO_UAH: 40.61,
-};
+const Farenheit = 33.8;
+const results = document.getElementById("results");
 
-const converterForm = document.querySelector("#currency-converter-form");
+Celsium.addEventListener("submit", Metamorphose);
 
-converterForm.addEventListener("submit", convertHandler);
 
-function convertHandler(event) {
+function Metamorphose(event) {
+  const Celsium = document.getElementById("Celsium");
   event.preventDefault();
 
-  const amount = Number(document.querySelector("#amount").value);
-  const currency = document.querySelector("#currency").value;
+  const amount = document.querySelector('#amount').value
 
-  let convertedAmount;
-
-  const { USD_TO_UAH: usdCourse, EUR_TO_UAH: eurCourse } = CURRENCY;
-
-  switch (currency) {
-    case "USD": {
-      convertedAmount = amount * usdCourse;
-      break;
-    }
-    case "EUR": {
-      convertedAmount = amount * eurCourse;
-      break;
-    }
-    default:
-      throw new Error("Something went wrong");
-  }
-
-  if (currency === "USD") {
-    convertedAmount = amount * usdCourse;
-  } else if (currency === "EUR") {
-    convertedAmount = amount * eurCourse;
-  } else {
-    throw new Error("Something went wrong");
-  }
-
-  const resultDiv = document.querySelector("#result");
-  resultDiv.innerHTML = `${amount} ${currency} = ${convertedAmount.toFixed(
-    2
-  )} UAH`;
+  let farenheitAmount = Farenheit * amount
+  results.innerHTML = `${amount} C = ${farenheitAmount} F`;
+  return farenheitAmount;
 }
